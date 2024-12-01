a68d314e1889c2cb15a2f0b64ab9b60cb904d439
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-01 15:40:19 +0100

    Dachs

---
+++ Mischwald.Library/Animals/Dachs.cs
@@ -0,0 +1,16 @@
+﻿using Mischwald.Trees;
+
+namespace Mischwald.Animals;
+
+/// <summary>
+/// Барсук
+/// </summary>
+public class Dachs : IAnimal, ILeftRight
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return 2;
+    }
+}
