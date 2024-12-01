21c0152a0efacf6a5851b668a12772fca6e00b91
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-01 15:42:01 +0100

    Stechmuecke

---
+++ Mischwald.Library/Insects/Stechmuecke.cs
@@ -0,0 +1,14 @@
+﻿using Mischwald.Bats;
+using Mischwald.Trees;
+
+namespace Mischwald.Insects;
+
+public class Stechmuecke : IInsect, ILeftRight
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return 1 * ctx.CurrentPlayer.GetAllCards<IBat>().Count;
+    }
+}
