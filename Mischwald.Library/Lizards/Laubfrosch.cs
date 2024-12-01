cf90ef755dbfe8d6abb3be689a23ef01bedf26bb
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-01 17:26:27 +0100

    Laubfrosch

---
+++ Mischwald.Library/Lizards/Laubfrosch.cs
@@ -0,0 +1,14 @@
+﻿using Mischwald.Insects;
+using Mischwald.Trees;
+
+namespace Mischwald.Lizards;
+
+public class Laubfrosch : ILizard, IBottom
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return 5 * ctx.CurrentPlayer.GetAllCards<Stechmuecke>().Count;
+    }
+}
