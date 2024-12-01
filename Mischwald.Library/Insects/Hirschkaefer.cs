30488f0405b0e3b9a4333d7a0da1d236240e14e7
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-01 10:05:51 +0100

    Hirschkaefer

---
+++ Mischwald.Library/Insects/Hirschkaefer.cs
@@ -0,0 +1,17 @@
+﻿using Mischwald.Animals;
+using Mischwald.Trees;
+
+namespace Mischwald.Insects;
+
+/// <summary>
+/// Жук-олень
+/// </summary>
+public class Hirschkaefer : IInsect
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return 1 * ctx.CurrentPlayer.GetAllCards<IAnimal>().Count;
+    }
+}
