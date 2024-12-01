8a348d3fe30a3869f9e33f771ddd19f2f56bf3cf
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-01 10:04:15 +0100

    Waldkauz

---
+++ Mischwald.Library/Birds/Waldkauz.cs
@@ -0,0 +1,13 @@
+﻿using Mischwald.Trees;
+
+namespace Mischwald.Birds;
+
+/// <summary>
+/// Сова
+/// </summary>
+public class Waldkauz : IBird
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx) => 5;
+}
