89aaeef29d077a4d2ea643b043d211053830370d
Dmitriy Vornychev <dm.s.vornychev@outlook.com>
2024-12-09 22:47:51 +0100

    Missing cards

---
+++ Mischwald.Library/Birds/Buchfink.cs
@@ -0,0 +1,13 @@
+﻿using Mischwald.Trees;
+
+namespace Mischwald.Birds;
+
+public class Buchfink : IBird
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return Parent is Buche ? 5 : 0;
+    }
+}
---
+++ Mischwald.Library/Plants/Moos.cs
@@ -0,0 +1,13 @@
+﻿using Mischwald.Trees;
+
+namespace Mischwald.Plants;
+
+public class Moos : IPlant, IBottom
+{
+    public T_Tree? Parent { get; set; }
+
+    public int GetInstancePoints(PointsCalculationContext ctx)
+    {
+        return ctx.CurrentPlayer.Trees.Count >= 10 ? 10 : 0;
+    }
+}
